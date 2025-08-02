2620a6092f1d04350f11acfacc7c507861a441d2
Norman Chizzali <nc222gs@student.lnu.se>
2025-08-02 12:22:49 +0200

    adding comments to session.d.ts file

--- backend/@types/session.d.ts
+++ backend/@types/session.d.ts
@@ -1,5 +1,10 @@
 import mongoose from "mongoose";
 
+/**
+ * This is the way to declare additional properties on the session object.
+ * In order to take effect, the declaration must be inside a file named like this file,
+ * and in a folder called @types so that typescript knows where to find it.
+ */
 declare module 'express-session' {
   interface SessionData {
     userId: mongoose.Types.ObjectId;
